c5d37edfbd3b6048aeab3c92107ebb97d126d1b5
leec <includeleec@gmail.com>
2019-10-23 23:26:41 +0800

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-licheng"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://2gxv79vhg0.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_fYxB3HoUQ",
+    APP_CLIENT_ID: "44gsvp73f9nh750agncv47i5b0",
+    IDENTITY_POOL_ID: "us-east-2:af763a61-cfe0-462d-a08a-5d8fec2cdb82"
+  }
+};
